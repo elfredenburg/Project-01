fed1f21512f57a4961de612797559b2a6c545c00
Elizabeth Fredenburg <efredenburg4065@mail.kvcc.edu>
2011-01-25 12:48:36 -0500

    Added Outbuffering Example To Project

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
 <?php
+
+/*
+ * @todo: DkBck fmt
+ * Elizabeth Fredenburg
+ * Project 1
+ * ANM 293: Advanced PHP
+ */
+
+// Initialise output buffering
+ob_start();
+
+// Set buffer contents
+echo "My First Project with Output Buffering";
+
+// Pull buffer contents
+$v = ob_get_contents();
+
+// Erase and turn off buffer
+ob_end_clean();
+
+// Output contents from buffer
+echo $v;
+
+?>
